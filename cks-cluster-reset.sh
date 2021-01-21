8fb2cf6044338421ee8d9701012be859156889c9
msftjonw <35560783+msftjonw@users.noreply.github.com>
2021-01-21 13:55:23 -0800

    Update cks-cluster-reset.sh

--- cks-cluster-reset.sh
+++ cks-cluster-reset.sh
@@ -1,6 +1,10 @@
 #reset with kubeadm
 kubeadm reset
 
+#remove kubeconfig
+#switch to the normal user
+rm -rf ~/.kube
+
 #reset iptables
 iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
 
