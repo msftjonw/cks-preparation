be5a265bd18a655009524ec01d0b0e7d32d2c120
msftjonw <35560783+msftjonw@users.noreply.github.com>
2021-01-26 13:11:59 -0800

    Update reset-and-initiate-k8s-cluster.sh

--- reset-and-initiate-k8s-cluster.sh
+++ reset-and-initiate-k8s-cluster.sh
@@ -1,5 +1,5 @@
 #reset with kubeadm
-kubeadm reset
+kubeadm reset -f
 
 #remove kubeconfig
 #switch to the normal user
@@ -8,5 +8,23 @@ rm -rf ~/.kube
 #reset iptables
 iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
 
-#dekete all files created in certain directory
-find . -type f -name "*.yaml" -delete
+#delete all files created in certain directory with something.yaml
+find /home/jonw -type f -name "*.yaml" -delete
+
+#delete all files created in certain directory not with something.yaml
+find /home/jonw -type f \! -name "*.yaml" -delete
+
+#initiate a new kubeadm K8s cluster
+kubeadm init
+
+## Switch to general user (not root)
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Initiate Pod network
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+# Show the command to add worker node to the cluster
+echo "### COMMAND TO ADD A WORKER NODE ###"
+kubeadm token create --print-join-command --ttl 0
