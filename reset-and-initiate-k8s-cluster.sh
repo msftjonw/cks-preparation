9c63786dbbecb2b7899418f221d82f4afa0de2c0
msftjonw <35560783+msftjonw@users.noreply.github.com>
2021-02-02 14:12:04 -0800

    Update reset-and-initiate-k8s-cluster.sh

--- reset-and-initiate-k8s-cluster.sh
+++ reset-and-initiate-k8s-cluster.sh
@@ -14,14 +14,15 @@ iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
 #initiate a new kubeadm K8s cluster
 kubeadm init
 
-## Switch to general user (not root)
+# switch to general user (not root)
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+# switch to root
 # Initiate Pod network
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
-# Show the command to add worker node to the cluster
+# show the command to add worker node to the cluster
 echo "### COMMAND TO ADD A WORKER NODE ###"
 kubeadm token create --print-join-command --ttl 0
